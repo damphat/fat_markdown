e102dab4345c0e4bee50ea10916fa5bd30a21e48
damphat <damphat@gmail.com>
2026-02-28 00:42:23 +0000

    Deploying to gh-pages from @ damphat/fat_markdown@d622cdf6a61a28babb28cceb935e66b89dc71d93 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2455986560" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "883179003" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
